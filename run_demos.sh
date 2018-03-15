35fa099d63276579339cf966f8995f0cbf456c26
Edward Vidal Jr <develone@sbcglobal.net>
2018-03-15 15:20:45 +0000

    The Raspbian RPi3B was created using the zip file that I created with pi-gen.     https://github.com/develone/pi-gen     Details to create the zip file with pi-gen are found     https://github.com/develone/raspbian-pi-gen.     The version provides the Lazarus IDE (Ultibo Edition)     https://github.com/develone/raspbian-pi-gen/blob/master/ultibo_raspbian.doc     Appendix A show the 11 steps required to create the zip file.     https://github.com/develone/raspbian-pi-gen/blob/master/first_boot.doc     provides the steps to get the Raspbian RPi3B ready to start developing     Ultibo project with just a few clicks.     See https://github.com/develone/raspbian-pi-gen/blob/master/lazarus_demo.doc
    The run_demo.sh gives the user the ability to change the Ultibo program without
    swapping out the micro sd card.
    
    IP of Ultibo systems are defined near the top of run_demo.sh
    
    RPI2="192.168.1.185"
    RPI3="192.168.1.200"
    
    ./run_demos.sh 185 jpeg
    tftping DWT_LIFT_RPi2 kernel7.img
    to ultibo system  192.168.1.185
    tftp> tftp> Sent 3111876 bytes in 13.5 seconds
    
    ./run_demos.sh 185 demo
    tftping UltiboDemoRPi2_TFTP kernel7.img
    to ultibo system  192.168.1.185
    tftp> tftp> Sent 2784448 bytes in 15.2 seconds
    
    ./run_demos.sh 185 playvideo
    tftping HelloVideo_TFTP kernel7.img
    to ultibo system  192.168.1.185
    tftp> tftp> Sent 3185412 bytes in 14.9 seconds
    
    ./run_demos.sh 185 servo
    tftping PWM_TFTP_SERVO kernel7.img
    to ultibo system  192.168.1.185
    tftp> tftp> Transfer timed out.
    
    In the event of ransfer timed out. Recycle the power on the Ultibo system.
    
    ./run_demos.sh 185 servo
    tftping PWM_TFTP_SERVO kernel7.img
    to ultibo system  192.168.1.185
    tftp> tftp> Sent 2743472 bytes in 10.1 seconds
    
    ./run_demos.sh 200 servo
    no servo  192.168.1.200
    
    ./run_demos.sh 185 video
    No camera  ultibo sytem  192.168.1.185
    
    ./run_demos.sh 200 demo
    tftping UltiboDemoRPi2_TFTP kernel7.img
    to ultibo system  192.168.1.200
    tftp> tftp> Sent 2784448 bytes in 9.5 seconds
    
    ./run_demos.sh 200 jpeg
    tftping DWT_LIFT_RPi2 kernel7.img
    to ultibo system  192.168.1.200
    tftp> tftp> Sent 3111876 bytes in 13.1 seconds

--- run_demos.sh
+++ run_demos.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+
+RPI2="192.168.1.185"
+RPI3="192.168.1.200"
  
 if [ $2 = "demo" ]; then
 	cd Demo/RPi2
 	if [ -e kernel7.img ]; then
 		if [ $1 = "185" ]; then
-			echo "tftping UltiboDemoRPi2_TFTP kernel7.img"
-			echo "to ultibo sytem 192.168.1.185"
-			tftp 192.168.1.185 < cmdstftp
+			echo "tftping UltiboDemoRPi2_TFTP kernel7.img" 
+			echo "to ultibo system " $RPI2
+			tftp $RPI2 < cmdstftp
 		else
 			echo "tftping UltiboDemoRPi2_TFTP kernel7.img"
-			echo "to ultibo sytem 192.168.1.200"
-			tftp 192.168.1.200 < cmdstftp
+			echo "to ultibo system " $RPI3
+			tftp $RPI3 < cmdstftp
 		fi
 	else	
 		echo "no kernel7.img"
@@ -21,18 +24,74 @@ else
 		cd RaspiVid_TFTP/RPi2/
 		if [ -e kernel7.img ]; then
 			if [ $1 = "185" ]; then
-				echo "tftping UltiboDemoRPi2_TFTP kernel7.img"
-				echo "No camera  ultibo sytem 192.168.1.185"
+				 
+				echo "No camera  ultibo sytem " $RPI2
 		
 			else
 				echo "tftping RaspiVid_TFTP kernel7.img"
-				echo "to ultibo sytem 192.168.1.200"
-				tftp 192.168.1.200 < cmdstftp
+				echo "to ultibo system " $RPI3
+				tftp $RPI3 < cmdstftp
 			fi
-	else	
-		echo "no kernel7.img"
-	fi	
-	cd ../../
+		else	
+			echo "no kernel7.img"
+		fi	
+		cd ../../
+	else
+			if [ $2 = "jpeg" ]; then
+				cd jpeg2000/RPi2
+				if [ -e kernel7.img ]; then
+					if [ $1 = "185" ]; then
+						echo "tftping DWT_LIFT_RPi2 kernel7.img"
+						echo "to ultibo system " $RPI2
+						tftp $RPI2 < cmdstftp
+		
+					else
+						echo "tftping DWT_LIFT_RPi2 kernel7.img"
+						echo "to ultibo system " $RPI3
+						tftp $RPI3 < cmdstftp
+					fi
+			else	
+				echo "no kernel7.img"
+			fi	
+			cd ../../
+		else
+			if [ $2 = "playvideo" ]; then
+				cd HelloPi/HelloVideo_TFTP/RPi2/
+				if [ -e kernel7.img ]; then
+					if [ $1 = "185" ]; then
+						echo "tftping HelloVideo_TFTP kernel7.img"
+						echo "to ultibo system " $RPI2
+						tftp $RPI2 < cmdstftp
+		
+					else
+						echo "tftping HelloVideo_TFTP kernel7.img"
+						echo "to ultibo system " $RPI3
+						tftp $RPI3 < cmdstftp
+					fi
+				else	
+					echo "no kernel7.img"
+				fi	
+				cd ../../../
+		else
+			if [ $2 = "servo" ]; then
+				cd Ultibo_PWM_TFTP/RPi2
+				if [ -e kernel7.img ]; then
+					if [ $1 = "185" ]; then
+						echo "tftping PWM_TFTP_SERVO kernel7.img"
+						echo "to ultibo system " $RPI2
+						tftp $RPI2 < cmdstftp
+		
+					else
+							 
+						echo "no servo " $RPI3
+							 
+					fi
+				else	
+					echo "no kernel7.img"
+				fi	
+				cd ../../
+			fi	
+		fi
 fi
 fi
-
+fi
