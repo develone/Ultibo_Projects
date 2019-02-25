447ecbc1b7e334db0d6c4d6daedd99a70734f4d0
Edward Vidal Jr <develone@sbcglobal.net>
2019-02-25 12:13:27 -0700

    testing Mark's zigtest code.

---
+++ ultibo_zig/zz.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#export PATH=~/testbuilds/ultibozig/zig-0.3.0+98869edb/build/bin:$PATH
+export PATH=~/testbuilds/Ultibo_Projects/ultibo_zig/zig-linux-x86_64-0.3.0+e5d4862e:$PATH
+echo $PATH
+which zig
