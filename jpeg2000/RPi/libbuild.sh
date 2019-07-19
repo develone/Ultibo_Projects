1f081a5b354f631a17720f4b01d215649d28463d
Ed Vidal Jr <devel@mypi3-11.attlocal.net>
2019-07-19 06:49:18 -0600

    now runs from devel not pi add documentation for jpeg 2000

--- jpeg2000/RPi/libbuild.sh
+++ jpeg2000/RPi/libbuild.sh
@@ -3,11 +3,14 @@
 rm -f dwtlift.o
 rm -f libdwtlift.a
 rm -f libopenjp2.a
-cp  libopenjp2_rpi_e357de.a libopenjp2.a 
+#cp  libopenjp2_816f53.a libopenjp2.a 
+cp  ../src/libopenjp2.a libopenjp2.a 
 
 arm-none-eabi-gcc -L. -llibopenjp2 -O2 -mabi=aapcs -marm -march=armv6 -mfpu=vfp -mfloat-abi=hard -c dwtlift.c
 
 arm-none-eabi-ar rcs libopenjp2.a dwtlift.o 
 
 cp libopenjp2.a libdwtlift.a
+arm-none-eabi-ar t libdwtlift.a > libdwtlift_obj.txt
+arm-none-eabi-objdump -d libdwtlift.a > libdwtlift.txt 
   
--- th_svd/RPi2/buildlib.sh
+++ th_svd/RPi2/buildlib.sh
@@ -17,4 +17,4 @@ arm-none-eabi-gcc -D_POSIX_THREADS -lpthread -O3 -mabi=aapcs -marm -march=armv7-
 #gcc test_svd.c svd.o disp_mat.o -lm -o test_svd
 arm-none-eabi-ar rcs libsvd.a *.o
 arm-none-eabi-ar -t libsvd.a > libsvd_obj.txt
-fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/RPI2.CFG -O4 svd_FS_RPi2.lpr
+fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/devel/ultibo/core/fpc/bin/RPI2.CFG -O4 svd_FS_RPi2.lpr
