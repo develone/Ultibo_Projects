585061c622f2024409c2b0b4c204a23ec6b9cf9e
Edward Vidal Jr <develone@sbcglobal.net>
2020-03-15 09:24:53 -0600

    initial implementation of ppp between pi-zero and RPi3B+

---
+++ slip-ppp/linux/scripts/ppp-3.1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#mypi3-1
+sudo stty -F /dev/ttyUSB0 raw
+sudo stty -F /dev/ttyUSB0 -a
+
+sudo pppd /dev/ttyUSB0 115200 10.0.5.1:10.0.5.2 noauth local debug dump defaultroute nocrtscts persist maxfail 0 holdoff 1
