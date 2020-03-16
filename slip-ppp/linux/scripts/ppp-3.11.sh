5bfd3344863321f8a9d09fd236602f2b6b73fcbb
Edward Vidal Jr <develone@sbcglobal.net>
2020-03-16 06:30:12 -0600

    1M baud devel@mypi3-11:~ $ time scp lena_rgb_1024.bmp pi@10.0.5.1:~/ lena_rgb_1024.bmp                             100% 3072KB  80.7KB/s   00:38
    real	0m39.651s
    user	0m0.929s
    sys	0m0.673s
    Tested that the image transferred successful.
    devel@mypi3-11:~ $ md5sum lena_rgb_1024.bmp
    5e987f94322785c2f90e63a82ac5b667  lena_rgb_1024.bmp
    pi@mypi3-1:~ $ md5sum lena_rgb_1024.bmp
    5e987f94322785c2f90e63a82ac5b667  lena_rgb_1024.bmp
    
    Updated ppp adding images of items interconnected.

--- slip-ppp/linux/scripts/ppp-3.11.sh
+++ slip-ppp/linux/scripts/ppp-3.11.sh
@@ -3,4 +3,4 @@
 sudo stty -F /dev/ttyUSB0 raw
 sudo stty -F /dev/ttyUSB0 -a
 
-sudo pppd /dev/ttyUSB0 230400 10.0.5.2:10.0.5.1 noauth local debug dump defaultroute nocrtscts persist maxfail 0 holdoff 1
+sudo pppd /dev/ttyUSB0 1000000 10.0.5.2:10.0.5.1 noauth local debug dump defaultroute nocrtscts persist maxfail 0 holdoff 1
