3775c68a0787434f26913a1a0a63bb5014f8500a
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-06-24 16:48:11 -0600

    I added q2mdl-wham & q2mdl-droideka to md2 folder on micro sd.
    Modified Testnanogl.c
    git diff Testnanogl.c
    
    diff --git a/software-rasterizer-ultibo-/RPi3/Testnanogl.c b/software-rasterizer-ultibo-/RPi3/Testnanogl.c
    index 58cb1ae..17e79b9 100644
    --- a/software-rasterizer-ultibo-/RPi3/Testnanogl.c
    +++ b/software-rasterizer-ultibo-/RPi3/Testnanogl.c
    @@ -395,6 +395,8 @@ static VECTOR4D vp = {0,0,0,1};
    
     // load the md2 object
     Load_Object_MD2(&obj_md2,                            // the loaded md2 file placed in container
    +     //"md2\\q2mdl-tekkblade\\tris.md2", //  "D:/Games/quakeII/baseq2/players/male/tris.md2",    // the filename of the .MD2 model^M
    +     //"md2\\q2mdl-tekkblade\\tris.md2", //  "D:/Games/quakeII/baseq2/players/male/tris.md2",    // the filename of the .MD2 model^M
          "md2\\q2mdl-tekkblade\\tris.md2", //  "D:/Games/quakeII/baseq2/players/male/tris.md2",    // the filename of the .MD2 model
          &vs,
          &vp,
    
    uncomment one of the lines 398, 399, or 400 in file
    Testnanogl.c.  Note: Only 1 of these lines should be uncommented
    Create a new libTestnanogl.a library with the command
    Created a script buildtechnanogl.sh to compile and create the library
    libTestnanogl.a
    
    ~/Ultibo_Projects/software-rasterizer-ultibo-/RPi3 $./buildtechnanogl.sh
    
    Create a new kernel7.img
    
    Transfer the kernel7.img to Ultibo System.
    
    tftp ww.xx.yy.zz < cmdstftp
    tftp> tftp> Sent 3754928 bytes in 40.1 seconds
    
    When the transfer is complete the Ultibo System will reboot

---
+++ software-rasterizer-ultibo-/RPi3/buildtechnanogl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -D__DYNAMIC_REENT__ -c Testnanogl.c
+arm-none-eabi-ar rcs libTestnanogl.a Testnanogl.o
