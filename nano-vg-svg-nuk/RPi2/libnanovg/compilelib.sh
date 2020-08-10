906d2084fe8183f827e65098c169898c65f72a60
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-08-10 10:58:55 -0600

    cleanup libnanovg/compilelib.sh

--- nano-vg-svg-nuk/RPi2/libnanovg/compilelib.sh
+++ nano-vg-svg-nuk/RPi2/libnanovg/compilelib.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-rm -f *.o libnanovg.a libnanovg-dis.txt ../libTestnanogl.a
+rm -f *.o libTestnanogl-dis.txt ../libTestnanogl.a
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -D__DYNAMIC_REENT__ -L. -I/opt/vc/include -lm -c Testnanogl.c 
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -D__DYNAMIC_REENT__ -I/opt/vc/include -c nanovg.c 
-arm-none-eabi-ar rcs libnanovg.a nanovg.o Testnanogl.o
 arm-none-eabi-ar rcs libTestnanogl.a nanovg.o Testnanogl.o
-arm-none-eabi-objdump -d nanovg.o > libnanovg-dis.txt
+arm-none-eabi-objdump -d libTestnanogl.a > libTestnanogl-dis.txt
 cp libTestnanogl.a ../
