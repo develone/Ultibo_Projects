13416ddb61eb2eb6d1c0e9bf07dbd471d2ac73c4
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-09-04 10:49:52 -0600

    now the question sym are replaced.

--- ultibo_nuklear/RPi3/dirent.h
+++ ultibo_nuklear/RPi3/dirent.h
@@ -1,18 +1,59 @@
-/*  dirent.h
- *
- */
-#ifndef DIRENT_H
-#define DIRENT_H
+/* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
 
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+   
+//Removed to work in ultibo
+//#ifndef _SYS_DIRENT_H
+//# error "Never use <bits/dirent.h> directly; include <dirent.h> instead."
+//#endif   
+   
+#ifndef DIRENT_H //changed
+#define DIRENT_H 
+
 
 struct dirent
-{
-    char        d_name[260];
-};
+  {
+#ifndef __USE_FILE_OFFSET64
+    __ino_t d_ino;
+    __off_t d_off;
+#else
+    __ino64_t d_ino;
+    __off64_t d_off;
+#endif
+    unsigned short int d_reclen;
+    unsigned char d_type;
+    char d_name[256];		/* We must not include limits.h! */
+  };
+
+#ifdef __USE_LARGEFILE64
+struct dirent64
+  {
+    __ino64_t d_ino;
+    __off64_t d_off;
+    unsigned short int d_reclen;
+    unsigned char d_type;
+    char d_name[256];		/* We must not include limits.h! */
+  };
+#endif
+
+#define d_fileno	d_ino	/* Backwards compatibility.  */
+
+// Changed for the nuklear example
+// filebrowser
 
 typedef struct
 {
@@ -29,8 +70,9 @@ struct dirent  * readdir  (DIR *dir);
 int            closedir (DIR *dir);
 void           rewinddir(DIR *dir);
 
-#ifdef __cplusplus
-}
-#endif
+#undef  _DIRENT_HAVE_D_NAMLEN
+#define _DIRENT_HAVE_D_RECLEN
+#define _DIRENT_HAVE_D_OFF
+#define _DIRENT_HAVE_D_TYPE
 
-#endif
+#endif //changed
