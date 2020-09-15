dfa56ea678a719ccd0a2e284bd66402e170bf27e
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-09-15 09:17:19 -0600

    testing for 50, 120, & 300 order 1 to 6

--- FFT/doc/fft-tests/filter-tests/butttf.m
+++ FFT/doc/fft-tests/filter-tests/butttf.m
@@ -78,16 +78,59 @@ plot(f,P1)
 %subplot(2,1,2)
 %plot(1000*t(1:50),x(1:50));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure
+for order = 1:3
 %|H(w)| = Ao/sqrt(1+(w/wo)2n
 ff =  (0:1000-1)*T;
 ww = 2*pi*ff;
-pow = 2*order;
+pow = 2*order
 ww = (ww/nf);
 ww = ww(1:1000).^pow;
 ww = sqrt(ww + 1);
 ww = (ww).^-1;
- 
-figure 
+
+%figure 
 %subplot(2,1,1)
+if order == 1 subplot(3,1,1) end
+if order == 2 subplot(3,1,2) end
+if order == 3 subplot(3,1,3) end
+%if order == 4 subplot(3,1,3) end
+%if order == 5 subplot(3,1,3) end
+%if order == 1 subplot(3,1,3) end
 plot((1:100),ww(1:100))
-grid minor on
+%ylabel('Magnitude (dB)')
+xlabel ('Freq') 
+
+
+
+%grid minor on
+end
+
+figure
+for order = 3:6
+%|H(w)| = Ao/sqrt(1+(w/wo)2n
+ff =  (0:1000-1)*T;
+ww = 2*pi*ff;
+pow = 2*order
+ww = (ww/nf);
+ww = ww(1:1000).^pow;
+ww = sqrt(ww + 1);
+ww = (ww).^-1;
+
+%figure 
+%subplot(2,1,1)
+%if order == 1 subplot(3,1,1) end
+%if order == 2 subplot(3,1,2) end
+%if order == 3 subplot(3,1,3) end
+if order == 4 subplot(3,1,1) end
+if order == 5 subplot(3,1,2) end
+if order == 6 subplot(3,1,3) end
+plot((1:100),ww(1:100))
+%ylabel('Magnitude (dB)')
+xlabel ('Freq') 
+
+
+
+%grid minor on
+end
+
