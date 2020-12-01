aca59c1663cd15d769d1199c0fad8a69bbeac811
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-12-01 13:50:47 -0700

    Now Lazarus is used to create kernel7.img following ./compile which creates libtest.a

--- dwt_lifting_step/RPi2/compile.sh
+++ dwt_lifting_step/RPi2/compile.sh
@@ -6,5 +6,5 @@ rm -f kernel7.img
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c test.c
 arm-none-eabi-ar rcs libtest.a test.o
 
-fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/devel/ultibo/core/fpc/bin/RPI2.CFG -O2 LibCTest_RPi2.lpr
+#fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/devel/ultibo/core/fpc/bin/RPI2.CFG -O2 LibCTest_RPi2.lpr
 ls -la test.o libtest.a kernel7.img
