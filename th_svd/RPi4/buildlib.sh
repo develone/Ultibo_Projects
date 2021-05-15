42a98efe37519d0a8867e5eee900b3fbc7c1a931
Edward Vidal Jr <develone@sbcglobal.net>
2021-05-15 10:06:51 -0600

    upgrading from RPi3 to RPi4

---
+++ th_svd/RPi4/buildlib.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#export PATH=/home/devel/ultibo/core/fpc/bin:$PATH
+rm -f *.o
+rm -f libsvd.a
+
+arm-none-eabi-gcc -I../include -O3 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c svd.c -o svd.o
+arm-none-eabi-gcc -I../include -O3 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c disp_mat.c -o disp_mat.o
+arm-none-eabi-gcc -I../include -O3 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c trans_mat.c -o trans_mat.o
+arm-none-eabi-gcc -I../include -O3 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c mul_mat.c -o mul_mat.o
+arm-none-eabi-gcc -D_POSIX_THREADS -lpthread -I../include -O3 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c mythread.c -o mythread.o
+arm-none-eabi-gcc -I../include -O3 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c pnmio.c -o pnmio.o
+arm-none-eabi-gcc -I../include -O3 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c error.c -o error.o
+ 
+ 
+echo "Compiling example ultibo_th_svd "
+arm-none-eabi-gcc -DUltibo -D_POSIX_THREADS -lpthread -I../include -O3 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c master.c -o ultibo_th_svd.o
+
+	
+#gcc test_svd.c svd.o disp_mat.o -lm -o test_svd
+arm-none-eabi-ar rcs libsvd.a *.o
+arm-none-eabi-ar -t libsvd.a > libsvd_obj.txt
+#fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI3B @/home/devel/ultibo/core/fpc/bin/RPI3.CFG -O4 svd_FS_RPi3.lpr
---
+++ th_svd/RPi4/master.c
@@ -0,0 +1,104 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<unistd.h>
+#include "../include/master_slave.h"
+
+
+#ifdef Ultibo
+void test_svd() {
+#else
+void main() {
+#endif	
+	
+	
+	char *inp1, *inp2, *inp3;
+	struct FILEs *files1 = (struct FILEs *)malloc(sizeof(struct FILEs));
+    struct FILEs *files2 = (struct FILEs *)malloc(sizeof(struct FILEs));
+    struct FILEs *files3 = (struct FILEs *)malloc(sizeof(struct FILEs));
+    
+    inp1 = "red.pgm";
+    inp2 = "grn.pgm";
+    inp3 = "blu.pgm";
+    
+    files1->input_file = inp1;
+    files2->input_file = inp2;
+    files3->input_file = inp3;
+
+    inp1 = "Sred.bin";
+    inp2 = "Sgrn.bin";
+    inp3 = "Sblu.bin";    
+ 
+    
+    files1->first_output = inp1;
+    files2->first_output = inp2;
+    files3->first_output = inp3;
+	
+    inp1 = "rcred.bin";
+    inp2 = "rcgrn.bin";
+    inp3 = "rcblu.bin";    
+ 
+    
+    files1->second_output = inp1;
+    files2->second_output = inp2;
+    files3->second_output = inp3;
+	
+    files1->status = 0;
+    files2->status = 0;
+    files3->status = 0;
+    
+    files1->num_bytes_rd = 0;
+    files2->num_bytes_rd = 0;
+    files3->num_bytes_rd = 0;
+    
+    files1->mem_allocated = 0;
+    files2->mem_allocated = 0;
+    files3->mem_allocated = 0;
+        
+    printf("In main %s %s %s %d %d\n",files1->input_file,files1->first_output,files1->second_output,files1->status,files1->mem_allocated);
+    printf("In main %s %s %s %d %d\n",files2->input_file,files2->first_output,files2->second_output,files2->status,files2->mem_allocated);
+    printf("In main %s %s %s %d %d\n",files3->input_file,files3->first_output,files3->second_output,files3->status,files3->mem_allocated);
+
+   
+    struct args *Allen = (struct args *)malloc(sizeof(struct args));
+    char f1[] = "files1";
+    char allen[] = "Allen";
+    Allen->name = allen;
+    Allen->age = 20;
+    
+
+    
+
+    pthread_t tid;
+    th_id[0] = 0;
+    th_id[1] = 0;
+    th_id[2] = 0;
+    pthread_create(&tid, NULL, hello, (void *)Allen);
+    pthread_join(tid, NULL);
+    printf("0x%x\n",th_id[0]);
+    pthread_create(&th_id[0], NULL, mysvd, (void *)files1);
+    
+	pthread_join(th_id[0], NULL);
+	
+	pthread_create(&th_id[1], NULL, mysvd, (void *)files2);
+	
+	pthread_join(th_id[1], NULL);
+
+	pthread_create(&th_id[2], NULL, mysvd, (void *)files3);
+
+	pthread_join(th_id[2], NULL);
+	printf("all threads joined\n");
+	 
+
+		printf("In main status %d num_bytes_rd %d \n",files1->status,files1->num_bytes_rd);
+		printf("In main status %d num_bytes_rd %d \n",files2->status,files2->num_bytes_rd);
+		printf("In main status %d num_bytes_rd %d \n",files3->status,files3->num_bytes_rd);
+	
+    
+    free(files1);
+    free(files2);
+    free(files3);
+    
+    free(Allen);
+    
+}
