1978af2c283c5ed012d8927b6c2c0a0c8ead24f8
Edward Vidal Jr <develone@sbcglobal.net>
2021-05-17 17:37:46 -0600

    adding missing files that are needed.

---
+++ jpeg2000/RPi4/writexxxcombin.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+int a, *pa;
+FILE *ptr;
+
+printf("%x\n",ptr);
+ptr = fopen("640480com","wb");
+pa=&(a);
+printf("pa = 0x%x \n", pa);
+a = 25;
+printf("Compression ratio a = 0x%x a = %d \n", a,a);
+fwrite(pa,1,sizeof(int),ptr);
+a = 1;
+printf("encode/decode a = 0x%x a = %d \n", a,a);
+fwrite(pa,1,sizeof(int),ptr);
+a = 0;
+printf("a = 0x%x \n", a);
+fwrite(pa,1,sizeof(int),ptr);
+a = 0;
+printf("a = 0x%x \n", a);
+fwrite(pa,1,sizeof(int),ptr);
+a = 640;
+printf("Width a = 0x%x a = %d \n", a,a);
+fwrite(pa,1,sizeof(int),ptr);
+a = 480;
+printf("Height a = 0x%x a = %d \n", a,a);
+fwrite(pa,1,sizeof(int),ptr);
+return 0;
+}
