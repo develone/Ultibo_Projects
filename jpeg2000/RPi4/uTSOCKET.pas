673b79463dccae561642f374434262d3220cd908
Edward Vidal Jr <develone@sbcglobal.net>
2021-06-04 20:53:34 -0600

    Changing BoundPort := 69; to BoundPort := 8069; sudo apt install ncat
    ncat -v -u 192.168.1.143 8069
    Ncat: Version 7.70 ( https://nmap.org/ncat )
    Ncat: Connected to 192.168.1.143:8069.
    
    git diff uTSOCKET.pas
    diff --git a/jpeg2000/RPi4/uTSOCKET.pas b/jpeg2000/RPi4/uTSOCKET.pas
    index c37fe89..0cae963 100644
    --- a/jpeg2000/RPi4/uTSOCKET.pas
    +++ b/jpeg2000/RPi4/uTSOCKET.pas
    @@ -184,7 +184,7 @@ begin
       Threads.Min := 5;
       Threads.Max := 10;
       BufferSize := 1024;
    -  BoundPort := 69;
    +  BoundPort := 8069;
       Transfers := TList.Create;
       FRebootOnImg := true;
       FOnMsg := nil;

--- jpeg2000/RPi4/uTSOCKET.pas
+++ jpeg2000/RPi4/uTSOCKET.pas
@@ -184,7 +184,7 @@ begin
   Threads.Min := 5;
   Threads.Max := 10;
   BufferSize := 1024;
-  BoundPort := 69;
+  BoundPort := 8069;
   Transfers := TList.Create;
   FRebootOnImg := true;
   FOnMsg := nil;
