16ba835181f1da8309bc534f644f26340061bf4e
Edward Vidal Jr <develone@sbcglobal.net>
2021-07-22 10:27:07 -0600

    removing -m 256 all on 1 line

--- filehandle/QEMU/startqemu.sh
+++ filehandle/QEMU/startqemu.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-qemu-system-arm -machine versatilepb -cpu cortex-a8 -kernel kernel.bin -m size=256 \
- -drive file=disk.img,if=sd,format=raw
+qemu-system-arm -machine versatilepb -cpu cortex-a8 -kernel kernel.bin -drive file=disk.img,if=sd,format=raw
