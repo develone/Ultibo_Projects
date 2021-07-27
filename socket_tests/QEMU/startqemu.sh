5da994514ae79f4466e9c29ff057316d2dc2d412
Edward Vidal Jr <develone@sbcglobal.net>
2021-07-27 06:45:51 -0600

    chg recommended by Ultibo

--- socket_tests/QEMU/startqemu.sh
+++ socket_tests/QEMU/startqemu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 qemu-system-arm -machine versatilepb -cpu cortex-a8 -kernel kernel.bin \
--net user,hostfwd=tcp::5080-:80,hostfwd=tcp::5023-:23,hostfwd=tcp::5069-:69,hostfwd=tcp::6050-:5050 -net nic \
+-net user,hostfwd=tcp::5080-:80,hostfwd=tcp::5023-:23,hostfwd=udp::5069-:69,hostfwd=tcp::6050-:5050 -net nic \
  -drive file=disk.img,if=sd,format=raw
