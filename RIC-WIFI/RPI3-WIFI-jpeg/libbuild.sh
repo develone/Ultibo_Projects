703a167ca2186a15ca47900ee268dc35289e0ba0
Ed Vidal Jr <devel@pi4-23.attlocal.net>
2021-12-07 13:30:33 -0700

    2 scripts are used to compile the openjpeg sources and the calling of commpress https://github.com/develone/Ultibo_Projects/blob/master/RIC-WIFI/src/compile_ultibo.sh
    https://github.com/develone/Ultibo_Projects/blob/master/RIC-WIFI/RPI3-WIFI-jpeg/libbuild.sh
    
    ./compile_ultibo.sh
    The word count here should be 22
    the word count in /home/pi/jpeg-2000-test/bare-metal/openjp
    when ./libbuild.sh is executed should be 22
     22  22 182 libopenjp2_obj.txt
    
    ./libbuild.sh
    dwtlift.c: In function 'decompress':
    dwtlift.c:658:3: warning: implicit declaration of function 'octave_write_byte'; did you mean 'opj_write_tile'? [-Wimplicit-function-declaration]
       octave_write_byte(r_decompress_fn,r_decompress,da_x1*da_y1);
       ^~~~~~~~~~~~~~~~~
       opj_write_tile
    
    After the openjpeg finished the compression for several sec the
    green led does not flash and the Status=MMC STATUS_TIMEOUT
    When the green led starts flashing The Status=MMC STATUS_TIMEOUT
    
    Transfer test.j2k to RPi4B
    tftp xx.xx.xx.xx
    tftp> binary
    tftp> get test.j2k
    Received 7848 bytes in 0.1 seconds
    tftp> quit
    
    build/bin/opj_decompress -i test.j2k -o 1207.png
    
    [INFO] Start to read j2k main header (0).
    [INFO] Main header has been correctly decoded.
    [INFO] No decoded area parameters, set the decoded area to the whole image
    [INFO] Header of tile 1 / 1 has been read.
    [INFO] Generated Outfile 1207.png
    decode time: 21 ms
    
    sha256sum kernel7.img
    da353c68d3e57eb40ddd280159604053c51e4a3fa610a9e1b0448cad138634ec  kernel7.img

--- RIC-WIFI/RPI3-WIFI-jpeg/libbuild.sh
+++ RIC-WIFI/RPI3-WIFI-jpeg/libbuild.sh
@@ -6,7 +6,7 @@ rm -f libopenjp2.a
 #cp  libopenjp2_816f53.a libopenjp2.a 
 cp  ../src/libopenjp2.a libopenjp2.a 
 
-arm-none-eabi-gcc -L. -llibopenjp2 -O2 -mabi=aapcs -marm -march=armv6 -mfpu=vfp -mfloat-abi=hard -c dwtlift.c
+arm-none-eabi-gcc -L. -llibopenjp2 -O3 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c dwtlift.c
 
 arm-none-eabi-ar rcs libopenjp2.a dwtlift.o 
 
