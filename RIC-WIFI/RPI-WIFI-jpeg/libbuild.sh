7f411c780f3a4be97d95b47bfe32ef680e409664
Ed Vidal Jr <devel@pi4-23.attlocal.net>
2021-12-07 15:39:24 -0700

    RPI-WIFI is not using the same compiled lib ../src it uses the one     in src     -O3 -mcpu=arm1176jzf-s -mabi=aapcs -marm -mfpu=vfp -mfloat-abi=hard     in libbuild.sh & -O3 -mcpu=arm1176jzf-s -mabi=aapcs -marm -mfpu=vfp -mfloat-abi=hard     in src/compile_ultibo.sh provided by ric355 same as previous commit

--- RIC-WIFI/RPI-WIFI-jpeg/libbuild.sh
+++ RIC-WIFI/RPI-WIFI-jpeg/libbuild.sh
@@ -4,9 +4,9 @@ rm -f dwtlift.o
 rm -f libdwtlift.a
 rm -f libopenjp2.a
 #cp  libopenjp2_816f53.a libopenjp2.a 
-cp  ../src/libopenjp2.a libopenjp2.a 
+cp  src/libopenjp2.a libopenjp2.a 
 
-arm-none-eabi-gcc -L. -llibopenjp2 -O2 -mabi=aapcs -marm -march=armv6 -mfpu=vfp -mfloat-abi=hard -c dwtlift.c
+arm-none-eabi-gcc -L. -llibopenjp2 -O3 -mcpu=arm1176jzf-s -mabi=aapcs -marm -mfpu=vfp -mfloat-abi=hard -c dwtlift.c
 
 arm-none-eabi-ar rcs libopenjp2.a dwtlift.o 
 
