d18f209a44316c21752d578bab6a329ce5ab25ff
Ed Vidal <develone@sbcglobal.net>
2022-02-06 17:59:54 -0700

    set path for pico & ultibo

---
+++ picoultibo.sh
@@ -0,0 +1,5 @@
+export ULTIBO1=/home/devel/ultibo/core
+export ULTIBO2=/home/devel/qemu-6.1.0-rpios/bin
+export PICO=/home/devel/local/openocd/bin
+export PATH=$ULTIBO1:$ULTIBO2:$PICO:$PATH
+echo $PATH
