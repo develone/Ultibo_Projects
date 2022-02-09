a305ca6680a6bfd2b57e05acd8aed43492877719
Ed Vidal <develone@sbcglobal.net>
2022-02-09 07:27:50 -0700

    git@github.com:develone/MATLAB-Octave.git which was forked from  https://github.com/TheAlgorithms/MATLAB-Octave with modification to run on Raspberry Pi

---
+++ imgconv/QEMU/steganography/bitstring.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int asciiValueToBinary(int asciiInput)
+{
+	int res = 0, i = 1, rem;
+        
+	while (asciiInput > 0)
+	{
+		rem = asciiInput % 2;
+		res = res + (i * rem);
+		asciiInput = asciiInput / 2;
+		i = i * 10;
+	}
+	//printf("%x\n",res);
+	return(res);
+}
+
+void processstr(char  *x) {
+int i,l;
+l=strlen(x); 
+int outstr[l]; 
+//printf("C %d %s\n",l,x);
+for(i=0;i<l;i++) {
+	printf("%d %08d ",i,asciiValueToBinary(*x));
+  //printf("%08d",asciiValueToBinary(*x));
+  outstr[i]=asciiValueToBinary(*x);
+	x++;
+}
+printf("\n");
+for(i=0;i<l;i++) printf("%08d",outstr[i]);
+printf("\n");
+}
+
+int main() {
+	char *p;
+	char a[]="Now we are engaged in a great ci";
+	p = a;
+	processstr(p);
+return (0);
+}
