a6d7e3c586910f3237dbfc7017b629a7575caf43
Edward Vidal Jr <develone@sbcglobal.net>
2022-05-09 19:07:42 -0600

    WIP

--- swd-pico/pico/compile.sh
+++ swd-pico/pico/compile.sh
@@ -8,3 +8,5 @@ arm-none-eabi-objdump -d swdloader.o > swdloader.dis
 arm-none-eabi-objdump -d ultibo-swd.o > ultibo-swd.dis
 arm-none-eabi-ar rcs libswd.a *.o
 arm-none-eabi-ar t libswd.a > libswd_obj.txt
+
+arm-none-eabi-objdump -d libswd.a > libswd.dis
