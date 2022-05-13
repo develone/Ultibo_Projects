376952d1495272701f0e8717ef87b34efa821aa1
Edward Vidal Jr <develone@sbcglobal.net>
2022-05-13 17:53:12 -0600

    You haven't looked closely enough at the example command lines we provided in our earlier post.
    The networking part of the command has changed significantly because of differences in the Pi emulation, here is what you have shown:
     -netdev user,id=net0,hostfwd=tcp::5080-:80,hostfwd=tcp::5023-:23,hostfwd=tcp::8050-:5050,hostfwd=udp::5069-:69,hostfwd=tcp::6050-:5050 -device usb-net,netdev=net0

--- Crypto-Socket/RPi3/QEMU/startqemu.sh
+++ Crypto-Socket/RPi3/QEMU/startqemu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 qemu-system-arm -machine raspi2b -cpu cortex-a8 -kernel kernel7.qimg \
--net user,hostfwd=tcp::5080-:80,hostfwd=tcp::5023-:23,hostfwd=tcp::8050-:5050,hostfwd=udp::5069-:69,hostfwd=tcp::6050-:5050 -net nic \
+-netdev user,id=net0,hostfwd=tcp::5080-:80,hostfwd=tcp::5023-:23,hostfwd=tcp::8050-:5050,hostfwd=udp::5069-:69,hostfwd=tcp::6050-:5050 -device usb-net,netdev=net0 \
  -drive file=disk.img,if=sd,format=raw
+
