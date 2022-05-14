a05ad57fda00500c91f81a123fcc90fd79a196f8
Edward Vidal Jr <develone@sbcglobal.net>
2022-05-13 20:39:32 -0600

    Now QEMU RPi3 works or on my Ultibo_Projects/Crypto-Socket/RPi3/QEMU/Svr.lpr
    git diff startqemu.sh
    diff --git a/Crypto-Socket/RPi3/QEMU/startqemu.sh b/Crypto-Socket/RPi3/QEMU/startqemu.sh
    index 2faf41c..e2ca45c 100755
    --- a/Crypto-Socket/RPi3/QEMU/startqemu.sh
    +++ b/Crypto-Socket/RPi3/QEMU/startqemu.sh
    @@ -1,4 +1,5 @@
     #!/bin/bash
     qemu-system-arm -machine raspi2b -cpu cortex-a8 -kernel kernel7.qimg \
    --net user,hostfwd=tcp::5080-:80,hostfwd=tcp::5023-:23,hostfwd=tcp::8050-:5050,hostfwd=udp::5069-:69,hostfwd=tcp::6050-:5050 -net nic \
    +-netdev user,id=net0,hostfwd=tcp::5080-:80,hostfwd=tcp::5023-:23,hostfwd=tcp::8050-:5050,hostfwd=udp::5069-:69,hostfwd=tcp::6050-:5050 -device usb-net,netdev=net0 \
      -drive file=disk.img,if=sd,format=raw
    
    devel@pi4-27:~/Ultibo_Projects/Crypto-Socket/RPi3/QEMU $ telnet xx.xx.xx.xx 6050
    Trying 192.168.1.211...
    Connected to 192.168.1.211.
    Escape character is '^]'.
    212345678901234567890123456789012:My Secret IV:My Extra Secret AAD:0e599f59da22536d4fe2d6bd48c118e594d7ee54ba178f1c918dfd69dea863b7514a8a263a5e5846b15de04945412b3f6334f78109d079d0f8198199c7a2ca297f376f1f97d1e3e75473538b943a6824b01cae711e8c1fedf0837660b99efc554ca60904d613420cf20af45831cd:3fe2f70affa859b4e9644875644878b2
    2
    Data received 212345678901234567890123456789012:My Secret IV:My Extra Secret AAD:0e599f59da22536d4fe2d6bd48c118e594d7ee54ba178f1c918dfd69dea863b7514a8a263a5e5846b15de04945412b3f6334f78109d079d0f8198199c7a2ca297f376f1f97d1e3e75473538b943a6824b01cae711e8c1fedf0837660b99efc554ca60904d613420cf20af45831cd:3fe2f70affa859b4e9644875644878b2
    
    MyData 0e599f59da22536d4fe2d6bd48c118e594d7ee54ba178f1c918dfd69dea863b7514a8a263a5e5846b15de04945412b3f6334f78109d079d0f8198199c7a2ca297f376f1f97d1e3e75473538b943a6824b01cae711e8c1fedf0837660b99efc554ca60904d613420cf20af45831cd 220
    MYKey 12345678901234567890123456789012 32
    AES GCM Decrypt Success
    Ascii PlainStr is The quick brown testing a longer string not dependent on length 15 1234567890 abcdefghijklmnopqrstuvwxyz012345
    
    still trying to map the usb in Ultibo_Projects/picotransfer/RPi3/QEMU/picoultibo.lpr project
    
    Bus 002 Device 001: ID 1d6b:0003 Linux Foundation 3.0 root hub
    Bus 001 Device 004: ID 2e8a:000a Raspberry Pi Pico
    Bus 001 Device 005: ID 046d:c534 Logitech, Inc. Unifying Receiver
    Bus 001 Device 003: ID 058f:6254 Alcor Micro Corp. USB Hub
    Bus 001 Device 002: ID 2109:3431 VIA Labs, Inc. Hub
    Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub
    
    -device usb-host,vendorid=0x2e8a,productid=0x000a using this did not work.

--- picotransfer/RPi3/QEMU/startqemu.sh
+++ picotransfer/RPi3/QEMU/startqemu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 qemu-system-arm -machine raspi2b -cpu cortex-a8 -kernel kernel7.qimg \
--net user,hostfwd=tcp::5080-:80,hostfwd=tcp::5023-:23,hostfwd=tcp::8050-:5050,hostfwd=udp::5069-:69,hostfwd=tcp::6050-:5050 -net nic \
- -drive file=disk.img,if=sd,format=raw -device usb-host,hostbus=1,hostaddr=3
+-netdev user,id=net0,hostfwd=tcp::5080-:80,hostfwd=tcp::5023-:23,hostfwd=tcp::8050-:5050,hostfwd=udp::5069-:69,hostfwd=tcp::6050-:5050 -device usb-net,netdev=net0 \
+ -drive file=disk.img,if=sd,format=raw --device usb-host,vendorid=0x2e8a,productid=0x000a
