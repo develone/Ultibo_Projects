886ba138e8b8a86b5229f8b3d777f22ba042146a
Edward Vidal Jr <develone@sbcglobal.net>
2022-05-19 06:20:57 -0600

    now add picotool to the path

--- picoultibo.sh
+++ picoultibo.sh
@@ -1,5 +1,6 @@
 export ULTIBO1=/home/devel/ultibo/core
 export ULTIBO2=/home/devel/qemu-6.2.0-rpios/bin
 export PICO=/home/devel/local/openocd/bin
-export PATH=$ULTIBO1:$ULTIBO2:$PICO:$PATH
+export PICOTOOL=/home/devel/picotool/build/
+export PATH=$ULTIBO1:$ULTIBO2:$PICO:$PICOTOOL:$PATH
 echo $PATH
