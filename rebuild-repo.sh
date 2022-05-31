3c82295804fb1d6aafd8c15ea1f24baf60f25f96
Edward Vidal Jr <develone@sbcglobal.net>
2022-05-31 08:22:53 -0600

    minor chgs

--- rebuild-repo.sh
+++ rebuild-repo.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 #this script is run with Ultibo_Projects/rebuild-repo.sh
-#the --recurse-submodule is needed to fetch the freertos repo.
+#the --recursive is needed to fetch the freertos repo.
+#the pico-sdk is needed with tinyUSB
 
 cd ~/
 
 rm -rf rp2040-freertos-project
 
-git clone  --recurse-submodules git@github.com:develone/rp2040-freertos-project
+git clone  --recursive git@github.com:develone/rp2040-freertos-project
 
 cd rp2040-freertos-project
 
-git clone git@github.com:develone/pico-sdk.git
-
-git clone https://github.com/raspberrypi/pico-sdk
-
-cd pico-sdk
-
-git submodule update --init
 
 cd ../
 
